86948435e586f857e3a5484813ba4a3dee74227d
Nedim Delic <nedimdelic@Nedims-MacBook-Pro.local>
2025-06-18 08:22:46 -0500

    Initial commit

---
+++ MasjidConnect/MasjidConnectApp.swift
@@ -0,0 +1,17 @@
+//
+//  MasjidConnectApp.swift
+//  MasjidConnect
+//
+//  Created by Nedim Delic on 6/18/25.
+//
+
+import SwiftUI
+
+@main
+struct MasjidConnectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MasjidConnectTests/MasjidConnectTests.swift
@@ -0,0 +1,17 @@
+//
+//  MasjidConnectTests.swift
+//  MasjidConnectTests
+//
+//  Created by Nedim Delic on 6/18/25.
+//
+
+import Testing
+@testable import MasjidConnect
+
+struct MasjidConnectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
